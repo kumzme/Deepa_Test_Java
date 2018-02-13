31b491934ee3ce0ef05e1399c220fa26b47d961c
kumzme <lokithorshield@gmail.com>
2018-02-12 21:22:18 -0500

    aa

--- src/test/java/Deepa/Driver/Test_02.java
+++ src/test/java/Deepa/Driver/Test_02.java
@@ -24,16 +24,11 @@ public class Test_02 {
         display_All_Values_In_Array(Files);
         ArrayList<String> file_paths =  list_file_path(currDir());
         display_All_Values_In_Array(file_paths);
-//        ArrayList<String> a2 =  file_Read(file_paths.get(4),"src");
         String a2  = searchArryList(file_paths,"src");
         l.info(a2);
-//        display_All_Values_In_Array(a2);
         JSONObject a3 = file_Read_Json(file_paths.get(4),"resources") ;
         display_All_Values_In_Array(a3);
         JSONObject j1 =  search_Write_Json("a");
-
-        //File aa = write_File_Json(op_Data_Folder,a3);
-//        l.info(aa);
         File a1 = write_File_Json(op_Data_Folder + "/OP_File1.json", j1);
         l.info(a1);
 

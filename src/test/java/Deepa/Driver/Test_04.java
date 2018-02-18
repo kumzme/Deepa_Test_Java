77bd849caf5e4854ec142f28f22d2a46ac9948fd
kumzme <kumzme@github.com>
2018-02-17 22:58:12 -0500

    New changes

--- src/test/java/Deepa/Driver/Test_04.java
+++ src/test/java/Deepa/Driver/Test_04.java
@@ -226,6 +226,8 @@ public static void condition_st(){
      l.info(monthName + "has number of days " + number_Of_days);
 
   }
+  //takes the user to provide a single character from the alphabet. Print Vowel or Consonant, depending on the user input. If the user input is not a letter (between a and z or A and Z), or is a string of length > 1, print an error message
+
 public static void main(String args[]){
 
     condition_st();

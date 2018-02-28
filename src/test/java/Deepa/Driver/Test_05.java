4d6916dabe8c61f0bfffece675984a663d8a3786
dpatri <kumzme@github.com>
2018-02-27 23:43:28 -0500

    New Exercise

--- src/test/java/Deepa/Driver/Test_05.java
+++ src/test/java/Deepa/Driver/Test_05.java
@@ -8,6 +8,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
+import static java.util.Collections.max;
 import static java.util.Collections.shuffle;
 
 public class Test_05 {
@@ -145,7 +146,75 @@ public class Test_05 {
         Collections.swap(a1,1,3);
         l.info("after swap:" +a1);
     }
+  //Java program to join two array lists.
+    public static void Collection_exercise_22_15(ArrayList<String>a1, ArrayList<String> a2){
+        ArrayList<String> a3 = new ArrayList<String>();
+        a3.addAll(a1);
+        a3.addAll(a2);
+        l.info("after joining to array lists: " +a3);
+    }
+   //Java program to clone an array list to another array list
+    public static void Collection_exercise_22_16(ArrayList<String>a1){
+        ArrayList<String> a2 = new ArrayList<String>();
+         a1.clone();
+         l.info("clone array list:" +a1.clone());
+    }
+    //Java program to empty an array list.
+    public static void Collection_exercise_22_17(ArrayList<String>a1){
+        a1.removeAll(a1);
+        l.info(a1);
+//        int i = a1.size()-1;
+//        for(int j = 0;j < i; j++){
+//            a1.remove(j);
+//            if(a1 == null){
+//                l.info("ArrayList is empty");
+//
+//            }
+//        }
+    }
+    //Java program to test an array list is empty or not
+    public static Boolean Collection_exerise_22_18(ArrayList<String>a1){
+      //return a1.isEmpty();
+       if (a1.size()> 0){
+           return false;
+       }
+       else {
 
+           return true;
+       }
+    }
+    //Java program to trim the capacity of an array list the current list size
+    public static void Collection_exercise_22_19(ArrayList<String> a2){
+        l.info("before Trim :"+ a2);
+        a2.trimToSize();
+        l.info("after Trim :"+ a2);
+    }
+    //Java program to increase the size of an array list
+    public static void Collection_exercise_22_20(ArrayList<String>a1){
+        l.info(a1);
+        l.info(a1.size() );
+        a1.ensureCapacity(10);
+        a1.add("15");
+        a1.add("17");
+        a1.add("18");
+        a1.add("20");
+        a1.add("21");
+        l.info(a1);
+        l.info(a1.size());
+    }
+    //Java program to replace the second element of a ArrayList with the specified element
+    public static void Collection_exercise_22_21(ArrayList<String> a1, String xx){
+        a1.set(1,xx);
+        l.info(a1);
+        l.info(a1.size());
+    }
+    //Java program to print all the elements of a ArrayList using the position of the elements
+    public static void Collection_exercise_22_22(ArrayList<String >a1){
+        for(int i = 0 ; i < a1.size();i++){
+            l.info(a1.get(i));
+
+        }
+    }
     public static void main(String[] args) {
         // String[] a1 = {"Red","Green","Blue" };
         ArrayList<String> a1 = new ArrayList<String>();
@@ -183,7 +252,17 @@ public class Test_05 {
         Boolean TT = Collection_exercise_22_13(maxa1,a1);
         l.info(TT);
         Collection_exercise_22_14(a1);
+        Collection_exercise_22_15(maxa1,a1);
+        Collection_exercise_22_16(a1);
+        Collection_exercise_22_17(a1);
+       // Boolean T1 = Collection_exercise_22_18(maxa1);
+        Collection_exercise_22_19(maxa1) ;
+        Collection_exercise_22_20(maxa1) ;
+        Collection_exercise_22_21(maxa1,"Deepa");
+        Collection_exercise_22_22(maxa1);
     }
+
+
 
 
 }

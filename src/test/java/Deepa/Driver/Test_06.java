3353e7761e7883adb3f1aa1decd4276eb68e880e
dpatri <kumzme@github.com>
2018-02-28 22:35:02 -0500

    New Exercise

---
+++ src/test/java/Deepa/Driver/Test_06.java
@@ -0,0 +1,16 @@
+package Deepa.Driver;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class Test_06
+{
+    private static final Logger l = LogManager.getLogger(Test_05.class.getName());
+    //Java program to append the specified element to the end of a linked list
+    public static void   Collection_LinkedList_22_01(){
+
+    }
+    public static void main(String[] args){
+
+   }
+}

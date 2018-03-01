9511bad6bfe6d99f974b937cb6e43f91672d55ab
dpatri <kumzme@github.com>
2018-03-01 07:32:47 -0500

    New Exercise

--- src/test/java/Deepa/Driver/Test_06.java
+++ src/test/java/Deepa/Driver/Test_06.java
@@ -34,7 +34,43 @@ public class Test_06
    // public static void Collection_LinkedList_22_04(LinkedList<String>a1){
      // Iterator it = list.descendingIterator(); }
 
+    //Java program to insert the specified element at the specified position in the linked list
+    public static void Collection_LinkedList_22_05(LinkedList<String>a1 ){
+        a1.add(2,"New Exeercise 11");
+        l.info(a1);
+        a1.add(2,"New exercise 111");
+        l.info(a1);
+    }
+    // Java program to insert elements into the linked list at the first and last position
+    public static void Collection_LinkedList_22_06(LinkedList<String>a1){
+        a1.addFirst("testing starts");
+        a1.addLast("testing ends");
+        l.info(a1);
+    }
+    //Java program to insert the specified element at the front of a linked list
+    //public static void Colllection_LinkedList_22_07(LinkedList<String>a1){
+    //    a1.offerFirst("testing Front");
+    //}
+    //Java program to insert the specified element at the end of a linked list.
+    //public static void Colllection_LinkedList_22_08(LinkedList<String>a1){
+    //    a1.offerLast("testing last");
+    //}
+    //Java program to insert some elements at the specified position into a linked list.
+    public static void Collection_LinkedList_22_09(LinkedList<String>a1){
+        LinkedList<String> a2 = new LinkedList<String>();
+        l.info("____________a2________");
+        a2.add("11111");
+        a2.add("new added Exercise22");
+        a2.add("new added Exercise21");
+        a2.add("new added Exercise22");
+        a2.add("new added Exercise21");
+        l.info("new LinkedList" +a1);
+        l.info("_____a2 end______");
+        a1.addAll(1,a1);
+        l.info(a1);
 
+    }
+    //Java program to get the first and last occurrence of the specified elements in a linked list
     public static void main(String[] args){
 
         LinkedList<String> a1 = new LinkedList<String>();
@@ -46,6 +82,15 @@ public class Test_06
         Collection_LinkedList_22_01(a1);
         Collection_LinkedList_22_02(a1);
         Collection_LinkedList_22_03(a1);
+        Collection_LinkedList_22_05(a1);
+        Collection_LinkedList_22_06(a1);
+        LinkedList<String> a11 = new LinkedList<String>();
+        a11.add("1");
+        a11.add(" 2");
+        a11.add(" 3");
+        a11.add("4");
+        a11.add(" 5");
+      Collection_LinkedList_22_09(a11);
 
    }
 }

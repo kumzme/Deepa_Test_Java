01a9c0f014df5e47231be8f8005c73393ed64198
dpatri <kumzme@github.com>
2018-03-01 23:55:18 -0500

    New Exercise

--- src/test/java/Deepa/Driver/Test_08.java
+++ src/test/java/Deepa/Driver/Test_08.java
@@ -3,6 +3,25 @@ package Deepa.Driver;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
+import java.util.TreeSet;
+
 public class Test_08 {
     private static final Logger l = LogManager.getLogger(Test_05.class.getName());
+
+    public static void Collection_Treeset(TreeSet<Integer > a1){
+        l.info(a1);
+        l.info(a1.isEmpty() );
+        l.info(a1.first());
+        l.info(a1.tailSet(2) );
+        l.info(a1.headSet(1));
+    }
+    public static void main(String[] args){
+        TreeSet<Integer> a1 = new TreeSet<Integer>();
+        a1.add(1);
+        a1.add(2);
+        a1.add(22);
+        a1.add(21);
+        a1.add(12);
+        Collection_Treeset(a1);
+    }
 }

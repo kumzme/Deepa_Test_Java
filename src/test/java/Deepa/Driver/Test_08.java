d7219967f5a566ada14617b3ff4e08a49a173438
dpatri <kumzme@github.com>
2018-03-01 23:49:36 -0500

    New Exercise

--- src/test/java/Deepa/Driver/Test_08.java
+++ src/test/java/Deepa/Driver/Test_08.java
@@ -1,4 +1,8 @@
 package Deepa.Driver;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 public class Test_08 {
+    private static final Logger l = LogManager.getLogger(Test_05.class.getName());
 }
